31612d795394276c4fb334f64ff5c88936dea038
Anthony Clayton <am.clayton2000@gmail.com>
2021-01-31 20:10:46 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/registerUser', function(){
         return view('showRegister');
 });
 Route::get('/login5', function(){
-    return view('login3');
+    return view('Login3');
 });
 
 
