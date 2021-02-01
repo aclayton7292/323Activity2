81e285f1a39da032b2c324746d550adf086ab7f7
Anthony Clayton <am.clayton2000@gmail.com>
2021-01-31 18:37:30 -0700

    h

--- CLC/${full.deploy.path}/resources/views/loginFailed.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.appmaster')
-@section('title', 'Login PAssed Page')
-@section('content')
-<h2>Login Failed</h2>
-<a href="login2">Try Again</a>
-@endsection
--- CLC/${full.deploy.path}/resources/views/loginFailure.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head><title>Login Failed</title></head>
-<body>
-<?php echo "You have failed successfuly at logging in"; ?>
-<p>Click <a href="login">here</a> to try again.</p>
-</body>
-
-
-</html>
